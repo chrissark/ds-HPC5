48773e5847ee2692819a10c4ee7cd3a969e9379a
chrissark <64375679+chrissark@users.noreply.github.com>
2021-07-08 20:49:03 +0300

    Create slurm_ds_gpt2.sh

---
+++ slurm_ds_gpt2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#SBATCH -D /s/ls4/users/kristina/nlp/GPT-2
+#SBATCH -o ./outputs/%j_ds_gpt2_nodes_5_gpus_4.out
+#SBATCH -e ./outputs/%j_ds_gpt2_nodes_5_gpus_4.err
+#SBATCH -p hpc5-gpu-3d
+#SBATCH --nodes 5
+#SBATCH --gres=gpu:k80:4
+#SBATCH --ntasks-per-node 4
+
+module load cuda/10.1
+
+export HOME=/s/ls4/users/kristina
+source $HOME/.bashrc
+conda activate $HOME/deepspeed_env
+
+mpirun python3 gpt2.py
